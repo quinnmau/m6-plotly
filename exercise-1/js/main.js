65f4951eeff6adab028785ca3a3a4fa8c9f5eeec
quinnmau <mauq@uw.edu>
2016-04-07 15:10:17 -0700

    finished

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -8,7 +8,23 @@ $(function() {
   }
 
   // Use d3.csv to read in your `data/Mass-Shooting-Data.csv` dataset: remember, you must be running a local server
-
+  d3.csv('data/Mass-Shooting-Data.csv', function(error, data) {
+      console.log(data);
+  
+    var data = [{
+        type: 'scattergeo',
+        locationmode: 'USA-states',
+        mode: 'markers',
+        lat: unpack(data, 'lat'),
+        lon: unpack(data, 'lng'),
+        text: unpack(data, 'injured').map(function(d) {return '<b>Injured</b>: ' + d}),
+        hoverinfo: 'text',
+        marker: {
+            size: unpack(data, 'injured').map(function(d) {return d*2}),
+            color: 'red',
+            opacity: .3
+        }
+    }];
 
     // Format your `data` object to pass to the plotly function. Make sure to set:
       // latitude (`lat`),
@@ -18,9 +34,25 @@ $(function() {
 
 
     // Declare your layout options to specify the title, projection and drawing specifications
-
+    var layout = {
+        title: 'Crowdsourced 2015 mass shootings',
+        showlegend: false,
+        geo: {
+            scope: 'usa',
+            projection: {
+                type: 'albers usa'
+            },
+            showland: true,
+            subunitwidth: 1,
+            landcolor: 'rgb(190, 190, 190)',
+            subunitcolor: 'white',
+            countrycolor: 'rgb(150, 150, 150)'
+        }
+    };
 
     // Call your Plotly function
-
+    Plotly.plot('map-div', data, layout);
   });
 });
+
+
